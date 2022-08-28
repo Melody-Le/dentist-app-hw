1b44613fee0e5f278dd92f1ce83b9ffb825eeb90
MUI LE <tanmui1992@gmail.com>
2022-08-28 17:23:16 +0800

    build

---
+++ dentist-assignment-react/build/precache-manifest.ea27c94e188ac9779043712f1f67b9c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0976d4147e3a931a797fb5ddfc91b8a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56668057585cae8b99e2",
+    "url": "/static/css/main.3be36971.chunk.css"
+  },
+  {
+    "revision": "52d4e9934a5ad85dd224",
+    "url": "/static/js/2.79050957.chunk.js"
+  },
+  {
+    "revision": "56668057585cae8b99e2",
+    "url": "/static/js/main.5a84b881.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
